6f811c77c0953b9a58cd8032e9063c5bce376e3d
Carlos Rodrigues <c.jrodrigues@hotmail.com>
2022-04-10 22:18:57 -0300

    Fix generate routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,11 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  generate: {
+    dir: 'docs',
+    routes: ['/'],
+  },
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
